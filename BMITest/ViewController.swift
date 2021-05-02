275d85dea645338977edafa08d55fd52375f0364
羅承志 <a2200099@yahoo.com>
2021-05-02 16:35:32 +0800

    Initial Commit

---
+++ BMITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMITest
+//
+//  Created by 羅承志 on 2021/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
